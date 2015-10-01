9686559c48d943551d3a147a01066d143245a92c
Glenn Hermans <glhermans@gmail.com>
2015-10-01 19:32:34 +0200

    Fixes #30

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,12 +1,15 @@
-@extends('app')
+@extends('welcome')
 
 @section('content')
-	<div class="row">
-		<div class="col-md-12">
-			<div class="panel panel-default">
-				<div class="panel-heading">Register</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
+<div class="container">
+<div class='loginColumns block block-size-login highlight-color-purple'>
+<div class='block-content-outer'>
+<div class='block-content-inner'>
+ <div class="row">
+      <div class="col-md-12">
+       <img src="{{ url('img/logo.png') }}" class="logo-name">
+         <div class="clearfix">&nbsp;</div>
+      			@if (count($errors) > 0)
 						<div class="alert alert-danger">
 							<strong>Whoops!</strong> There were some problems with your input.<br><br>
 							<ul>
@@ -21,36 +24,36 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Name</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+							<label for="name" class="col-md-5 control-label">Name</label>
+							<div class="col-md-7">
+								<input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+							<label for="email" class="col-md-5 control-label">E-Mail Address</label>
+							<div class="col-md-7">
+								<input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+							<label for="password" class="col-md-5 control-label">Password</label>
+							<div class="col-md-7">
+								<input type="password" class="form-control" id="password" name="password">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Confirm Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
+							<label for="confirm_pass" class="col-md-5 control-label">Confirm Password</label>
+							<div class="col-md-7">
+								<input type="password" class="form-control" id="confirm_pass" name="password_confirmation">
 							</div>
 						</div>
 
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
+								<button type="submit" class="btn btn-custom">
 									Register
 								</button>
 							</div>
@@ -60,5 +63,6 @@
 			</div>
 		</div>
 	</div>
+</div>
 </div>
 @endsection
